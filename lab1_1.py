552edd8572280d81e4d76632517ddff5414e72b3
Snake-GGJJWP <vladiksnake@gmail.com>
2025-03-13 23:42:19 +0600

    added computation for interval data

--- lab1_1.py
+++ lab1_1.py
@@ -12,7 +12,9 @@ def uniform_distribution(n: int):
 
 # Use CLT
 def normal_distribution(n: int):
-    return [(sum(uniform_distribution(CLT_SUM)) - CLT_SUM / 2) / (CLT_SUM / 12) ** 0.5 for _ in range(n)]  # normalized
+    m = CLT_SUM / 2  # uniform distribution mean
+    q = CLT_SUM / 12  # uniform distribution standard deviation
+    return [(sum(uniform_distribution(CLT_SUM)) - m) / q for _ in range(n)]  # normalized
 
 
 def get_interval_frequencies(data: list[float], num_bins: int) -> list[float]:
@@ -25,8 +27,11 @@ def get_interval_frequencies(data: list[float], num_bins: int) -> list[float]:
 
     for x in data:
         bin_ind = int((x - min_value) // bin_width)
-        bin_ind -= int(bin_ind == num_bins)  # correct for max_value
+        bin_ind -= int(bin_ind == num_bins)  # correct error for max_value
         count_bins[bin_ind] += 1
+
+    freqs = [count / (N * bin_width) for count in count_bins]
+    print(f"SUM OF BINS: {sum([freq*bin_width for freq in freqs])}")
 
     return [count / (N * bin_width) for count in count_bins]
 
@@ -39,62 +44,17 @@ def get_histogram_edges(data: list[float], num_bins: int):
 
 
 def model_histogram(x: list[float], y: list[float], width):
-    print(x)
-    print(y)
     plt.bar(x, y, width=width, align='edge', color='b', edgecolor='black')
-
-# REWRITE THIS SHIT
-
-
-# def show_histo(data: list[float], num_bins: int):
-#     N = len(data)  # number of RV
-#     len_bin = (max(data) - min(data)) / num_bins  # length of single bin
-#     i = min(data)
-#     arr_bins = [[i, i := (i + len_bin)] for _ in range(num_bins)]  # array of intervals [start; end]
-#     arr_bins[-1][1] = max(data)  # eliminate error (python can't calculate precisely)
-#     # print(arr_bins)
-
-#     # Frequency calculation
-#     arr_heights = []
-#     bin_ind = 0
-#     count = 0
-#     data_sorted = sorted(data)
-#     i = 0
-#     while i < N:
-#         if data_sorted[i] > arr_bins[bin_ind][1]:
-#             arr_heights.append(count / (N * len_bin))
-#             bin_ind += 1
-#             count = 0
-#             continue
-#         count += 1
-#         i += 1
-#     else:
-#         arr_heights.append(count / (N * len_bin))
-
-#     # arr_heights and arr_bins alignment
-#     for _ in range(len(arr_bins) - len(arr_heights)):
-#         arr_heights.append(0)
-
-#     # print(len(arr_bins))
-#     # print(len(arr_heights))
-#     # print(data_sorted)
-#     print(arr_heights)
-
-#     bin_starts = [b[0] for b in arr_bins]  # x positions
-#     bin_widths = [len_bin for b in arr_bins]  # Widths
-#     plt.bar(bin_starts, arr_heights, width=bin_widths, align='edge', color='b', edgecolor='black')
-#     # plt.show()
 
 
 def model_histogram_by_bins(data: list[float], num_bins: int):
-    # DO SOMETHING WITH THE REPEATING CODE!
     max_value = max(data)
     min_value = min(data)
     bin_width = (max_value - min_value) / num_bins
 
     freqs = get_interval_frequencies(data, num_bins)
     starts = get_histogram_edges(data, num_bins)
-    starts.pop() # omit the most right edge
+    starts.pop()  # omit the most right edge
     model_histogram(starts, freqs, bin_width)
 
 
@@ -103,20 +63,18 @@ def model_histogram_with_stetgers(data: list[float]):
     model_histogram_by_bins(data, num_bins)
 
 
-# def show_histo_with_stetgers(data: list[float]):
-#     num_bins = int(1 + log(len(data), 2))
-#     show_histo(data, num_bins)
-
-
 if __name__ == '__main__':
     N = 1000
 
     plt.xlabel('Value')
     plt.ylabel('Density')
-    plt.title(f'Histogram of {N} Normally Distributed Values')
+    plt.title(f'Histogram of {N} Uniform Distributed Values')
 
-    # arr = uniform_distribution(N)
-    # show_histo(arr, 10)
+    arr = uniform_distribution(N)
+    model_histogram_by_bins(arr, 10)
+    plt.show()
+
+    plt.title(f'Histogram of {N} Normally Distributed Values')
 
     arr = normal_distribution(N)
     model_histogram_by_bins(arr, 50)
--- lab1_2.py
+++ lab1_2.py
@@ -17,6 +17,7 @@ def nd_func(x: float, m: float, q: float):
     return coef * E ** power
 
 
+# Rejection sampling
 def get_random_values_by_truncation(n: int, x_min, x_max, func):
     data = []
     while len(data) < n:
@@ -27,8 +28,12 @@ def get_random_values_by_truncation(n: int, x_min, x_max, func):
 
     return data
 
+###############################
+# FUNCTION FOR DISCRET VALUES #
+###############################
 
-def get_mode(x: list[float]):
+
+def get_mode_discret(x: list[float]):
     if not x:
         return 0
 
@@ -48,9 +53,11 @@ def get_mode(x: list[float]):
     return mode
 
 
-def get_median(x: list[float]):
+def get_median_discret(x: list[float]):
     if not x:
         return 0
+
+    x = sorted(x)
 
     middle = len(x) // 2
 
@@ -60,14 +67,14 @@ def get_median(x: list[float]):
     return x[middle]
 
 
-def get_raw_moment(x: list[float], order=1):
+def get_raw_moment_discret(x: list[float], order=1):
     return sum([k**order for k in x]) / len(x)
 
 
-def get_moment(x: list[float], order=1):
+def get_moment_discret(x: list[float], order=1):
     variance = 0
 
-    m = get_raw_moment(x)
+    m = get_raw_moment_discret(x)
     n = len(x)
 
     for el in x:
@@ -78,24 +85,121 @@ def get_moment(x: list[float], order=1):
     return variance
 
 
-def get_unbiased_variance(x: list[float]):
+def get_unbiased_variance_discret(x: list[float]):
     n = len(x)
-    return get_moment(x, 2) * n / (n - 1)
+    return get_moment_discret(x, 2) * n / (n - 1)
 
 
-def get_standard_deviation(x: list[float]):
-    return get_unbiased_variance(x)**0.5
+def get_standard_deviation_discret(x: list[float]):
+    return get_unbiased_variance_discret(x)**0.5
 
 
-def get_asymmetry(x: list[float]):
-    nu_3 = get_moment(x, 3)
-    q = get_standard_deviation(x)
+def get_asymmetry_discret(x: list[float]):
+    nu_3 = get_moment_discret(x, 3)
+    q = get_standard_deviation_discret(x)
     return nu_3 / q**3
 
 
-def get_excess(x: list[float]):
-    nu_4 = get_moment(x, 4)
-    q = get_standard_deviation(x)
+def get_excess_discret(x: list[float]):
+    nu_4 = get_moment_discret(x, 4)
+    q = get_standard_deviation_discret(x)
+    return nu_4 / q**4 - 3
+
+###############################
+# FUNCTIONS FOR INTERVAL DATA # (note that we assume that intervals are the same width)
+###############################
+
+
+def get_mode_interval(x: list[float], y: list[float]):
+    if not x or len(x) != len(y):
+        return 0
+
+    n = len(x)
+    bin_width = x[1] - x[0]  # width of an interval
+
+    yM = 0
+    iM = 0  # mode bin index
+    for i in range(n):
+        if yM < y[i]:
+            yM = y[i]
+            iM = i
+
+    y_curr = y[iM]
+    y_next = y[iM + 1]
+    y_prev = y[iM - 1]
+
+    # We find the tallest bin and to find the mode point we intersect diagonals of a trapezium
+    # made from the higher points of the tallest bin and the left and right higher points of previous and next bin respectively
+    # thus the mode point will be drawn towards the taller neighboring bin
+    mode = x[iM] + bin_width * ((y_curr - y_prev) / ((y_curr - y_prev) + (y_curr - y_next)))
+    return mode
+
+
+def get_median_interval(x: list[float], y: list[float]):
+    # IDEA: we should traverse through X values until we reach the cumulative sum of
+    # bins' areas of more than 0.5. Then we find the Xm such that its rectangle adds up to exactly 0.5
+
+    if not x or len(x) != len(y):
+        return 0
+
+    n = len(x)
+    bin_width = x[1] - x[0]  # width of an interval
+
+    area = 0
+    i = 0
+    # Until the area hits 0.5 we accumulate bins' areas
+    while i < n:
+        if area + bin_width * y[i] >= 0.5:
+            break
+        area += bin_width * y[i]
+        i += 1
+
+    median = (0.5 - area) / y[i] + x[i]
+    return median
+
+
+def get_raw_moment_interval(x: list[float], y: list[float], order=1):
+    n = len(x)
+    bin_width = x[1] - x[0]
+    x_middles = [k + bin_width / 2 for k in x]  # middles of each bins
+
+    y_sum = sum(y)
+    y_normalized = [y[i] / y_sum for i in range(n)]
+    return sum([y_normalized[i] * x_middles[i]**order for i in range(n)])
+
+
+def get_moment_interval(x: list[float], y: list[float], order=1):
+    n = len(x)
+    bin_width = x[1] - x[0]
+    x_middles = [k + bin_width / 2 for k in x]
+
+    m = get_raw_moment_interval(x, y, 1)
+
+    y_sum = sum(y)
+    y_normalized = [y[i] / y_sum for i in range(n)]  # Don't we need the sum of all probabilities to be equal 1? Well, here we make it real.
+
+    variance = sum([y_normalized[i] * (x_middles[i] - m)**order for i in range(n)])
+    return variance
+
+
+def get_unbiased_variance_interval(x: list[float], y: list[float], n: int):
+    return get_moment_interval(x, y, 2) * n / (n - 1)
+
+
+# Following functions use UNBIASED variance so they need to know the total number of random values.
+def get_standard_deviation_interval(x: list[float], y: list[float], n: int):
+    return get_unbiased_variance_interval(x, y, n)**0.5
+
+
+def get_asymmetry_interval(x: list[float], y: list[float], n: int):
+    nu_3 = get_moment_interval(x, y, 3)
+    q = get_standard_deviation_interval(x, y, n)
+    return nu_3 / q**3
+
+
+def get_excess_interval(x: list[float], y: list[float], n: int):
+    nu_4 = get_moment_interval(x, y, 4)
+    q = get_standard_deviation_interval(x, y, n)
     return nu_4 / q**4 - 3
 
 
@@ -106,10 +210,9 @@ if __name__ == '__main__':
     k = 3
 
     data_user = get_random_values_by_truncation(n, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
-    data_50 = get_random_values_by_truncation(50, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
-    data_500 = get_random_values_by_truncation(500, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
-    data_1000 = get_random_values_by_truncation(1000, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
-    # print(sorted([int(x * 100) / 100 for x in data]))
+    # data_50 = get_random_values_by_truncation(50, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
+    # data_500 = get_random_values_by_truncation(500, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
+    # data_1000 = get_random_values_by_truncation(1000, m - q * k, m + q * k, lambda x: nd_func(x, m, q))
     model_histogram_with_stetgers(data_user)
 
     x = np.arange(m - k * q, m + k * q, 0.02)
@@ -121,19 +224,33 @@ if __name__ == '__main__':
     edges = get_histogram_edges(data_user, num_bins)  # that's our x
     middles = [(edges[i] + edges[i - 1]) / 2 for i in range(1, len(edges))]  # calculate middle of each bin
 
-    data_discret = []
-    for i in range(num_bins):
-        data_discret += [middles[i]] * int(freqs[i] * 100)
+    freq_sum = sum(freqs)
+    freq_normalized = [freqs[i] / freq_sum for i in range(len(freqs))]
 
-    my_mode_idiot = get_mode(data_user)
-    my_mode = get_mode(data_discret)
-    my_median = get_median(data_discret)
-    my_mean = get_raw_moment(data_discret, 1)
-    my_variance = get_moment(data_discret, 2)
-    my_unbiased_variance = get_unbiased_variance(data_discret)
-    my_standard_deviation = get_standard_deviation(data_discret)
-    my_excess = get_excess(data_discret)
-    my_asymmetry = get_asymmetry(data_discret)
+    data_discret = []
+    bin_width = (max(data_user) - min(data_user)) / n
+    for i in range(num_bins):
+        data_discret += [middles[i]] * int(freq_normalized[i] * 1000)
+
+    my_mode_idiot_discret = get_mode_discret(data_user)
+    my_mode_discret = get_mode_discret(data_discret)
+    my_median_discret = get_median_discret(data_discret)
+    my_mean_discret = get_raw_moment_discret(data_discret, 1)
+    my_variance_discret = get_moment_discret(data_discret, 2)
+    my_unbiased_variance_discret = get_unbiased_variance_discret(data_discret)
+    my_standard_deviation_discret = get_standard_deviation_discret(data_discret)
+    my_excess_discret = get_excess_discret(data_discret)
+    my_asymmetry_discret = get_asymmetry_discret(data_discret)
+
+    edges.pop()  # remove last edge. That's our x
+    my_mode_interval = get_mode_interval(edges, freqs)
+    my_median_interval = get_median_interval(edges, freqs)
+    my_mean_interval = get_raw_moment_interval(edges, freqs, 1)
+    my_variance_interval = get_moment_interval(edges, freqs, 2)
+    my_unbiased_variance_interval = get_unbiased_variance_interval(edges, freqs, n)  # additionally we pass number of elements in all intervals
+    my_standard_deviation_interval = get_standard_deviation_interval(edges, freqs, n)
+    my_excess_interval = get_excess_interval(edges, freqs, n)
+    my_asymmetry_interval = get_asymmetry_interval(edges, freqs, n)
 
     mode_idiot = statistics.mode(data_user)  # if not discret than the function will just find the most repeated value like an idiot
     mode = statistics.mode(data_discret)
@@ -145,16 +262,28 @@ if __name__ == '__main__':
     excess = scipy.stats.kurtosis(data_discret)  # Fisher definition is used. (3rd semester's flashbacks...)
     asymmetry = scipy.stats.skew(data_discret)
 
-    print(f"My data:")
-    print(f"Mode_idiot: {my_mode_idiot}")
-    print(f"Mode: {my_mode}")
-    print(f"Median: {my_median}")
-    print(f"Mean: {my_mean}")
-    print(f"Variance: {my_variance}")
-    print(f"Unbiased variance: {my_unbiased_variance}")
-    print(f"Standard deviation: {my_standard_deviation}")
-    print(f"Excess (kurtosis): {my_excess}")
-    print(f"Asymmetry (skew): {my_asymmetry}")
+    print(f"My data (discret):")
+    print(f"Mode_idiot: {my_mode_idiot_discret}")
+    print(f"Mode: {my_mode_discret}")
+    print(f"Median: {my_median_discret}")
+    print(f"Mean: {my_mean_discret}")
+    print(f"Variance: {my_variance_discret}")
+    print(f"Unbiased variance: {my_unbiased_variance_discret}")
+    print(f"Standard deviation: {my_standard_deviation_discret}")
+    print(f"Excess (kurtosis): {my_excess_discret}")
+    print(f"Asymmetry (skew): {my_asymmetry_discret}")
+
+    print()
+
+    print(f"My data (interval):")
+    print(f"Mode: {my_mode_interval}")
+    print(f"Median: {my_median_interval}")
+    print(f"Mean: {my_mean_interval}")
+    print(f"Variance: {my_variance_interval}")
+    print(f"Unbiased variance: {my_unbiased_variance_interval}")
+    print(f"Standard deviation: {my_standard_deviation_interval}")
+    print(f"Excess (kurtosis): {my_excess_interval}")
+    print(f"Asymmetry (skew): {my_asymmetry_interval}")
 
     print()
 
